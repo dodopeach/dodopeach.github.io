f6776fa205925995ec057c894502fd75a6a006ea
guzus <50664161+guzus@users.noreply.github.com>
2022-06-18 18:41:42 +0900

    exit from Juneung IT

--- src/components/Career.js
+++ src/components/Career.js
@@ -7,7 +7,7 @@ function Career() {
       <h2>guzus | Career</h2>
       <ul>
         <li>NAVER corporation, backend engineering, July 2021 ~ August 2021</li>
-        <li>Juneung IT, backend engineering, October 2021 ~ </li>
+        <li>Juneung IT, backend engineering, October 2021 ~ June 2022</li>
       </ul>
     </>
   );
